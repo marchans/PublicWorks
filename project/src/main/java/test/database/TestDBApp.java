920f837c5c199bde126d87ce9ba2f29719e0696a
Vera Baklanova <vira.baklanova@gmail.com>
2019-04-14 13:41:38 +0300

    Fix adding customer entry to database

--- project/src/main/java/test/database/TestDBApp.java
+++ project/src/main/java/test/database/TestDBApp.java
@@ -3,8 +3,10 @@ package test.database;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import database.data.roles.Customer;
 import database.data.users.Login;
 import database.data.users.User;
+import database.workers.CustomersWorker;
 import database.workers.LoginsWorker;
 import database.workers.UsersWorker;
 
@@ -20,16 +22,33 @@ public class TestDBApp {
 		
 		UsersWorker uw = (UsersWorker) context.getBean("usersWorker");
 		uw.addUser(u);
-//		
+		
 		Login lu = new Login();
 		lu.setUser(u);
-		lu.setLogin("jayStar");
+		lu.setLogin("CustomerTest3");
 		lu.setPassword("boom");
 		lu.setRole("ROLE_USER");
 		
 		LoginsWorker lw = (LoginsWorker) context.getBean("loginsWorker");
 		lw.addLogin(lu);
-	
+		
+		Customer cu = new Customer();
+		cu.setUser(u);
+		cu.setAccountNumber(01);
+		cu.setAptNumber(1);
+		cu.setArea(33);
+		cu.setCity("Kiev");
+		cu.setCounty("Kiev");
+		cu.setDistrict("Podil");
+		cu.setItn(3323667);
+		cu.setNumber(3);
+		cu.setPassport("A54467");
+		cu.setStreet("Khreshchatyk");
+		
+		System.out.println(cu);
+//		
+		CustomersWorker cuw = (CustomersWorker) context.getBean("customersWorker");
+		cuw.addCustomer(cu);
 	}
 
 }
