f1107ef751aa4d7b74647cb91cf4088ed3793884
marchans <35569253+marchans@users.noreply.github.com>
2019-04-14 21:46:29 +0300

    spring security

---
+++ project/src/main/java/publicworks/config/core/SpringSecurityInitializer.java
@@ -0,0 +1,7 @@
+package publicworks.config.core;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
